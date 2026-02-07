318d3b9e4c348a1df4d1b9c51f6af407270ae77f
MickNate <michaelnathanmitchell@gmail.com>
2026-02-07 00:22:37 -0500

    added textboxs and labels to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,11 @@
 export default function Home(){
     return (
         <p>
-            Welcome back to Concert Project! Please login below.
+            Welcome back to Concert Project! Please login below.<br/>
             <label id="usernameInput">Username: </label>
-            <input type="text" id="usernameInput" placeholder="Username here"/>
+            <input type="text" id="usernameInput" placeholder="Username here"/><br/>
             <label id="passwordInput">Password: </label>
-            <input type="text" id="passwordInput" placeholder="Password here"/>
+            <input type="text" id="passwordInput" placeholder="Password here"/><br/>
         </p>
     );
 }
