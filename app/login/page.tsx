a0b8f3220c3558359c724bf2827f9ff98e2b5c59
MickNate <michaelnathanmitchell@gmail.com>
2026-02-07 00:36:35 -0500

    added a login button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,8 @@
 export default function Home(){
+
+    function loginTest() {
+        return "/login"
+    }
     return (
         <p>
             Welcome back to Concert Project! Please login below.<br/>
@@ -6,7 +10,7 @@ export default function Home(){
             <input type="text" id="usernameInput" placeholder="Username here"/><br/>
             <label id="passwordInput">Password: </label>
             <input type="text" id="passwordInput" placeholder="Password here"/><br/>
-            <button>Test</button><br/>
+            <button onClick = {loginTest}>Test</button><br/>
         </p>
     );
 }
