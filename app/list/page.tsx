0fac7e18740da17d724c145c3ed5ce0111ca38af
MickNate <michaelnathanmitchell@gmail.com>
2026-02-08 04:43:52 -0500

    Reading the names in a text file and displaying them.

---
+++ app/list/page.tsx
@@ -0,0 +1,14 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export default async function List() {
+    const filePath = path.join(process.cwd(), 'userbase.txt'); // Get absolute path to the file
+    const fileContents = await fs.readFile(filePath, 'utf8'); // Read file as a string
+
+    return (
+        <div>
+            <h1>Current Users are:</h1>
+            <pre>{fileContents}</pre>
+        </div>
+    );
+}
