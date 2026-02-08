84a45d4d7a37480db23085cd6de0df4e59cb7764
MickNate <michaelnathanmitchell@gmail.com>
2026-02-08 18:05:04 -0500

    displaying user list

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,13 +1,17 @@
 export default function List() {
-    const users = ["first", "second", "third"]
-    return (
+    //const users = ["first", "second", "third"]
+    //let leng = users.length
+
+    return(
         <div>
-            <h1>Current Users:</h1>
-            <ul>
-                {users.map((name, index) => (
-                    <li key={index}>{name}</li>
-                ))}
-            </ul>
+            <h1>User List: </h1>
+            <p id="demo"></p>
+
+            <script>
+                const users = ["Saab", "Volvo", "BMW"];
+                document.getElementById("demo").innerHTML = users.toString();
+            </script>
         </div>
     );
+
 }
