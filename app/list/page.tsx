f645408c6102d162a788bea04649d24583a3d7c0
MickNate <michaelnathanmitchell@gmail.com>
2026-02-08 05:50:55 -0500

    Reading the names in a text file and displaying them.

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs';
 
 export default async function List() {
-    const file = await fs.readFile('userdatabase.txt', 'utf8');
+    const file = await fs.readFile('C:\\Users\\unkno\\OpenSourceWebTech\\concert_project\\app\\userdatabase.txt', 'utf8');
 
     return (
         <div>
