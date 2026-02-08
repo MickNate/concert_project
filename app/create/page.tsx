b19c9675cda2763c9bde568735010bdf2bc902e5
MickNate <michaelnathanmitchell@gmail.com>
2026-02-08 00:05:02 -0500

    adding creation form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -5,15 +5,34 @@ export default function Create(){
                 Welcome to Concert Project! Please fillout the information below to create your profile.
             </p>
             <p>
-                <form action="/submit_page.php" method="POST">
+                <form>
                     <label htmlFor="username">Username:</label><br/>
                     <input type="text" id="username" name="username"/><br/>
                     <label htmlFor="password">Password:</label><br/>
                     <input type="text" id="password" name="password"/><br/><br/>
-                    <input type="button" value="Create"/>
+                    <input type="button" value="Create" onClick={saveUser}/>
                 </form>
             </p>
         </div>
 
     );
+    function saveUser(){
+        let creButton = document.getElementById("button")
+        let creUser = document.getElementById("username")
+        let crePass = document.getElementById("password")
+
+        // @ts-ignore
+        creButton.addEventListener("click", () => {
+            // @ts-ignore
+            let userInp = creUser.value
+            // @ts-ignore
+            let passInp = crePass.value
+            let blobdtMIME =
+                new Blob([userInp, passInp], { type: "text/plain" })
+            let url = URL.createObjectURL(blobdtMIME)
+            let anele = document.createElement("a")
+            anele.href = url;
+            anele.click();
+        })
+    }
 }
