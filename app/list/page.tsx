c3830cb5726205029282be0d83a382f3b243d464
MickNate <michaelnathanmitchell@gmail.com>
2026-02-09 00:16:03 -0500

    displaying user list

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -2,19 +2,26 @@
 
 export default function List() {
 
-    const reptiles: string[] = ["alligator", "snake", "lizard"];
+    const users: string[] = ["first", "second", "third"];
+
+    let baseUrl = "https://concert-project.vercel.app/profile/guestview/"
 
     return(
         <body>
         <h1>User List: </h1>
         <ul>
-            {reptiles.map((reptile, index) => (
+            {users.map((user, index) => (
                 // Use a unique key for each item, the index can be used if the list is static
-                <li key={index}>{reptile}</li>
+                <li key={index}>getLink(user)</li>
             ))}
         </ul>
         </body>
     );
+
+    function getLink(user:string){
+        let fullUrl = baseUrl.concat(user)
+        return "<a href = " + fullUrl + "></a>"
+    }
 
 
 }
