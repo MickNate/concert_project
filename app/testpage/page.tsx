dca02a94330631e3486f3be2c90c72c1ef54577b
MickNate <michaelnathanmitchell@gmail.com>
2026-02-16 18:03:52 -0500

    testing reading a json file

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,7 +1,12 @@
-export default function Home(){
+import { promises as fs } from 'fs';
+
+export default async function Test(){
+    const file = await fs.readFile(process.cwd() + 'member_list.json', 'utf8');
+    const data = JSON.parse(file);
+
     return (
-        <p>
-            Testing the linking of a new page.
-        </p>
+        <div>
+            <p>{data.username}</p>
+        </div>
     );
 }
