cbda32472643cee60855bc8a605b87aaff74a2b4
MickNate <michaelnathanmitchell@gmail.com>
2026-02-16 04:42:02 -0500

    made the profile page dynamic and the login button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default function Login(){
 
     function loginUser() {
 
-        let logUser = document.getElementById("username")
+        let logUser = document.getElementById("usernameInput")
 
         // @ts-ignore
         let userInp = logUser.value
