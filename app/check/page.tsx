703a93beab69fe34e0c3ed0207ee8ed399206fe1
MickNate <michaelnathanmitchell@gmail.com>
2026-02-17 07:02:46 -0500

    guestview user page

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -1,39 +1,35 @@
-/*import { promises as fs } from 'fs';
+export default async function Check() {
 
-export default async function Check(){
+    /*    const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+        const logUser = urlParams.get('user')
+        const logPass = urlParams.get('pass')
 
-    const logUser = urlParams.get('user')
-    const logPass = urlParams.get('pass')
+        const file = await fs.readFile('member_list.json', 'utf8');
+        const data = JSON.parse(file);
+        const baseurl = "https://concert-project.vercel.app/profile/ownerview/"
 
-    const file = await fs.readFile('member_list.json', 'utf8');
-    const data = JSON.parse(file);
-    const baseurl = "https://concert-project.vercel.app/profile/ownerview/"
+        let temp = null
+        let temp2 = "Click here to try again."
+        let templink = "https://concert-project.vercel.app/profile/login/"
 
-    let temp = null
-    let temp2 = "Click here to try again."
-    let templink = "https://concert-project.vercel.app/profile/login/"
-
-    let user = data.find((item: { username: string | null; }) => item.username === logUser)
-    if(user == null){
-        temp = "The username you input does not exist"
-    }
-    else{
-        if(user.password.innerHTML === logPass) {
-            temp = "Welcome back, " + logUser + "!"
-            templink = baseurl.concat(user.username)
-            temp2 = "Click here to continue"
+        let user = data.find((item: { username: string | null; }) => item.username === logUser)
+        if(user == null){
+            temp = "The username you input does not exist"
         }
         else{
-            temp = "Sorry, the password you input is incorrect"
-        }
-    }
+            if(user.password.innerHTML === logPass) {
+                temp = "Welcome back, " + logUser + "!"
+                templink = baseurl.concat(user.username)
+                temp2 = "Click here to continue"
+            }
+            else{
+                temp = "Sorry, the password you input is incorrect"
+            }
+        }*/
 
-    return(
-        <div>
-            <p>{temp}</p>
-            <a href={templink}>{temp2}</a>
-        </div>
-    );*/
+    return (
+        <p>This will implement the login check once serverside added</p>
+    )
+}
