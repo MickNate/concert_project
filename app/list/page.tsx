186e7b948935d09cd5d6c8444567566790293081
MickNate <michaelnathanmitchell@gmail.com>
2026-02-17 06:06:39 -0500

    login page checks for user information

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -29,29 +29,3 @@ export default async function List() {
         return fullUrl
     }
 }
-
-/*export default function List() {
-
-    const users: string[] = ["first", "second", "third"];
-
-    let baseUrl = "https://concert-project.vercel.app/profile/guestview/"
-
-    return(
-        <body>
-        <h1>User List: </h1>
-        <ul id="userList">
-            {users.map((user, index) => (
-                // Use a unique key for each item, the index can be used if the list is static
-                    <li key={index}><a href={getLink(user)}>{user}</a></li>
-            ))}
-        </ul>
-        </body>
-    );
-
-    function getLink(user:string){
-        let fullUrl = baseUrl.concat(user)
-        return fullUrl
-    }
-
-
-}*/
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,6 @@
 
 export default async function Login(){
 
-    const response = await fetch("member_list.json");
-    const data = await response.json();
-
     return (
         <div>
             <p>
@@ -29,24 +26,15 @@ export default async function Login(){
         let logUser = document.getElementById("usernameInput")
         let logPass = document.getElementById("passwordInput")
 
-        let user = data.find((item: { name: string; password: string}) => item.name === JSON.stringify(logUser))
-
-        if(user){
+        if((logUser != null) && (logPass != null)){
             // @ts-ignore
-            nameMatch = (logUser.innerHTML == user.name)
-            // @ts-ignore
-            passMatch = (logPass.innerHTML == user.password)
-            // @ts-ignore
-            if(nameMatch && passMatch){
-                // @ts-ignore
-                let userInp = logUser.value
-                let baseUrl = "https://concert-project.vercel.app/profile/ownerview/"
-                let fullUrl = baseUrl.concat(userInp)
-                window.location.href = fullUrl
-            }
-        }
-        else{
-            alert("Username and/or password incorrect.")
+            localStorage.setItem("usertemp",logUser.innerHTML)
+            localStorage.setItem("userpass",logPass.innerHTML)
+            /*let userInp = logUser.innerHTML
+            let baseUrl = "https://concert-project.vercel.app/profile/ownerview/"
+            let fullUrl = baseUrl.concat(userInp)
+            window.location.href = fullUrl*/
+            window.location.href = "https://concert-project.vercel.app/check"
         }
     }
 }
