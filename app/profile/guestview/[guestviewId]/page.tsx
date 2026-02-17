a5b87e2fe25bc5dcb3ae1e2dbaca99027eeb1e31
MickNate <michaelnathanmitchell@gmail.com>
2026-02-17 07:19:03 -0500

    guestview user page

--- app/profile/guestview/[guestviewId]/page.tsx
+++ app/profile/guestview/[guestviewId]/page.tsx
@@ -18,7 +18,7 @@ export default async function GuestView( { params }: {
             <p>
                 {user.info}
             </p>
-            <p>{user.concerts}</p>
+            <p>{user.concerts.headliner}</p>
 
         </div>
     );
