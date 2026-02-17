776c4942d6c0f798145feefeb857451f41069287
MickNate <michaelnathanmitchell@gmail.com>
2026-02-16 19:31:59 -0500

    testing reading a json file

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default async function Test() {
-    const file = await fs.readFile(process.cwd() + '/../../member_list.json', 'utf8');
+    const file = await fs.readFile('member_list.json', 'utf8');
     const data = JSON.parse(file);
 
     return (
