261110de256b3f3e887d749cda3efae360e07321
MickNate <michaelnathanmitchell@gmail.com>
2026-02-17 06:55:35 -0500

    guestview user page

--- app/profile/guestview/[guestviewId]/page.tsx
+++ app/profile/guestview/[guestviewId]/page.tsx
@@ -1,12 +1,21 @@
+import { promises as fs } from 'fs';
+
 export default async function GuestView( { params }: {
     params: Promise<{ guestviewId: string}>;
 }){
     const guestviewID = (await params).guestviewId;
+
+    const file = await fs.readFile('member_list.json', 'utf8');
+    const data = JSON.parse(file);
+
+    let user = data.find((item: { name: string; }) => item.name === guestviewID)
+
     return (
         <div>
             <p>
-                This is the guest view of the profile for {guestviewID}.
+                This is the guest view of the profile for {user.username}.
             </p>
+            <p>{user.info}</p>
 
         </div>
     );
