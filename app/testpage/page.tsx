a99a66c717a8a4a18d4f2e2be978e944f2b900b2
MickNate <michaelnathanmitchell@gmail.com>
2026-02-16 19:41:54 -0500

    testing reading a json file

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,5 +1,6 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
 
 export default async function Test() {
     const file = await fs.readFile('member_list.json', 'utf8');
@@ -7,8 +8,14 @@ export default async function Test() {
 
     return (
         <div>
-            <h1>Username: {data.username}</h1>
-            <p>Info: {data.info}</p>
+            <h1>Data from JSON File (Server)</h1>
+            <ul>
+                {data.map((item: { username: Key | null | undefined; info: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) => (
+                    <li key={item.username}>
+                        <h2>{item.info}</h2>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
