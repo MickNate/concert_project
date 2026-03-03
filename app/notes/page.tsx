fddf671a22df0c0abbbb6b4ce1c3a253d35f58ae
MickNate <michaelnathanmitchell@gmail.com>
2026-03-03 07:58:50 -0500

    testing supabase connection

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,8 +5,12 @@ export default async function Notes(){
 // @ts-ignore
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
 
-const { data, error } = await supabase
+    const { data, error } = await supabase
     .from('todos')
     .select()
+
+    const { data: notes } = await supabase.from("notes").select();
+
+    return <pre>{JSON.stringify(notes, null, 2)}</pre>
 
 }
