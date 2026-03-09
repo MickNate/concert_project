34a139ba49d75d2117f5afeac218a5ca2680369c
MickNate <michaelnathanmitchell@gmail.com>
2026-03-09 06:25:27 -0400

    testing supabase connection

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Notes() {
     const supabase = await createClient();
-    const { data: notes } = await supabase.from("concert_users").select('user_name');
+    const { data: notes } = await supabase.from("concert_users").select('username');
 
     return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
