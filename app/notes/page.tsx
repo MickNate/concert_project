1cc537aacff07f4a3a4b801c869396e1db919999
MickNate <michaelnathanmitchell@gmail.com>
2026-03-09 05:24:08 -0400

    testing supabase connection

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,34 +1,8 @@
-import { createClient, QueryResult, QueryData, QueryError } from '@supabase/supabase-js'
+import { createClient } from '@/utils/supabase/server';
 
 export default async function Notes() {
-    
-    
-    const theURL = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobHN2ZmdqYWxraG1rcnZheGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1Mjc0MTYsImV4cCI6MjA4ODEwMzQxNn0.Ws_iNziFJfdfvTi5TS9-7paqwysxerLvEcCLo_JXwUw";
-    const theURL2 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobHN2ZmdqYWxraG1rcnZheGFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1Mjc0MTYsImV4cCI6MjA4ODEwMzQxNn0.Ws_iNziFJfdfvTi5TS9-7paqwysxerLvEcCLo_JXwUw"
-    const supabase = createClient(theURL, theURL2)
+    const supabase = await createClient();
+    const { data: notes } = await supabase.from("notes").select();
 
-    const usersQuery = supabase.from("concert_users").select();
-    type Users = QueryData<typeof usersQuery>;
-
-    //this is an update
-    const {data, error} = await usersQuery;
-    if (error) throw error;
-    const users: Users = data;
-
-    return (
-        <body>
-        <div>
-            <ul>
-                {users.map((user) => (
-                    <li key={user.id}>
-                        <h2>{user.username}</h2>
-                        <p>{user.password}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-        </body>
-    );
-
-
+    return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
