4db6d77b190e87f6d7c473c474cebaaaee8d1e25
MickNate <michaelnathanmitchell@gmail.com>
2026-03-24 04:52:31 -0400

    updating bio

--- app/edit/editbio/[editbioId]/bioEdit.ts
+++ app/edit/editbio/[editbioId]/bioEdit.ts
@@ -14,7 +14,7 @@ export async function bioEdit(previousState: string, formData: FormData){
         const supabase = await createClient();
         const { error } = await supabase
             .from('concert_user')
-            .update({ name: bio})
+            .update({ name: "second"})
             .eq('bio_info', "Testing if this updates the bio")
         if (error)
             return "Error: " + error.code + " : " + error.message;
