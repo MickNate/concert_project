90719b80de02377b4db4807287e23e6f2abe5cb3
MickNate <michaelnathanmitchell@gmail.com>
2026-03-27 01:32:15 -0400

    adding ability to add concerts

--- app/addcon/[addconId]/conAdd.ts
+++ app/addcon/[addconId]/conAdd.ts
@@ -1,1 +1,30 @@
 "use server";
+
+import {createClient} from "@/utils/supabase/server";
+
+export async function conAdd(previousState: string, formData: FormData){
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const user = formData.get("user") as string;
+    const head = formData.get("head") as string;
+    const other = formData.get("other") as string;
+    const venue = formData.get("venue") as string;
+    const tour = formData.get("tour") as string;
+    const showdate = formData.get("showdate") as string;
+
+    if((user == null) || (user == "")){
+        return "Error: User doesn't exist."
+    }
+    if((head == null) || (head == "")){
+        return "No input for headliner."
+    }
+
+    const supabase = await createClient();
+    const { error } = await supabase
+        .from('concert_users')
+        .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue, date_of: showdate })
+
+    if (error)
+        return "Error: " + error.code + " : " + error.message;
+    else
+        return "Successfully created!";
+}
--- app/addcon/[addconId]/page.tsx
+++ app/addcon/[addconId]/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import {useParams} from "next/navigation";
+import {useActionState} from "react";
+import {conAdd} from "@/app/addcon/[addconId]/conAdd";
 
 export default function AddCon() {
     const params = useParams<{ addconId: string }>()
     const userId = params.addconId;
+
+    const [error, action, isLoading] = useActionState(conAdd, "");
 
     return(
         <body>
@@ -12,7 +16,7 @@ export default function AddCon() {
             <p>Tell us about the concert you went to!</p>
         </div>
         <div>
-            <form>
+            <form action={action}>
                 <input type="hidden" id="user" name="user" value={userId}/>
                 <label htmlFor="head">Headliner:</label><br/>
                 <input type="text" id="head" name="head"/><br/>
@@ -20,7 +24,7 @@ export default function AddCon() {
                 <input type="text" id="other" name="other"/><br/>
                 <label htmlFor="tour">Tour:</label><br/>
                 <input type="text" id="tour" name="tour"/><br/>
-                <label htmlFor="showdate">Show date:</label>
+                <label htmlFor="showdate">Show date:</label><br/>
                 <input
                     type="date"
                     id="showdate"
@@ -29,7 +33,8 @@ export default function AddCon() {
                     max="2030-12-31"/><br/>
                 <label htmlFor="venue">Venue:</label><br/>
                 <input type="text" id="venue" name="venue"/><br/>
-                <button>Add</button>
+                <button>{isLoading ? "Creating..." : "Create"}</button>
+                {error && <p>{error}</p>}
             </form>
         </div>
         </body>
