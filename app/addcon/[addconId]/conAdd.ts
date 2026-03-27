c46a83149c1027eb4457e57b23c9108c201ab8e7
MickNate <michaelnathanmitchell@gmail.com>
2026-03-27 02:12:06 -0400

    adding ability to add concerts

--- app/addcon/[addconId]/conAdd.ts
+++ app/addcon/[addconId]/conAdd.ts
@@ -6,7 +6,7 @@ export async function conAdd(previousState: string, formData: FormData){
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const user = formData.get("user") as string;
     const head = formData.get("head") as string;
-    const other = formData.get("other") as string;
+    let other = formData.get("other") as string;
     const venue = formData.get("venue") as string;
     const tour = formData.get("tour") as string;
     const showdate = formData.get("showdate") as string;
@@ -18,13 +18,26 @@ export async function conAdd(previousState: string, formData: FormData){
         return "No input for headliner."
     }
 
+    other = "{" + other + "}";
+
     const supabase = await createClient();
 
-    const { error } = await supabase
-        .from('concert_concerts')
-        .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue})
-    if (error)
-        return "Error: " + error.code + " : " + error.message;
-    else
-        return "Successfully created!";
+    if(showdate == ""){
+        const { error } = await supabase
+            .from('concert_concerts')
+            .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue})
+        if (error)
+            return "Error: " + error.code + " : " + error.message;
+        else
+            return "Successfully created!";
+    }
+    else {
+        const { error } = await supabase
+            .from('concert_concerts')
+            .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue, date_of: showdate})
+        if (error)
+            return "Error: " + error.code + " : " + error.message;
+        else
+            return "Successfully created!";
+    }
 }
