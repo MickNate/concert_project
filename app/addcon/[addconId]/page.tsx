0c5a89e3bb23a295d1d240bb5d33537cfc5af923
MickNate <michaelnathanmitchell@gmail.com>
2026-03-27 00:54:00 -0400

    adding ability to add concerts

---
+++ app/addcon/[addconId]/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {useParams} from "next/navigation";
+
+export default function AddCon() {
+    const params = useParams<{ addconId: string }>()
+    const userId = params.addconId;
+
+    return(
+        <body>
+        <div>
+            <p>Tell us about the concert you went to!</p>
+        </div>
+        <div>
+            <form>
+                <input type="hidden" id="user" name="user" value={userId}/>
+                <label htmlFor="head">Headliner:</label><br/>
+                <input type="text" id="head" name="head"/><br/>
+                <label htmlFor="other">Other Artists:</label><br/>
+                <input type="text" id="other" name="other"/><br/>
+                <label htmlFor="tour">Tour:</label><br/>
+                <input type="text" id="tour" name="tour"/><br/>
+                <label htmlFor="showdate">Show date:</label>
+                <input
+                    type="date"
+                    id="showdate"
+                    name="showdate"
+                    min="1900-01-01"
+                    max="2030-12-31"/>
+                <label htmlFor="venue">Venue:</label><br/>
+                <input type="text" id="venue" name="venue"/><br/>
+                <button>Add</button>
+            </form>
+        </div>
+        </body>
+    );
+}
--- app/edit/editconcert/[editconcertId]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function EditConcert(){
-    return(
-        <p>You are not supposed to be here.</p>
-    );
-}
