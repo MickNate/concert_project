51ccca6b78f805fb8613536a3dbe07f7bb996ccf
MickNate <michaelnathanmitchell@gmail.com>
2026-03-27 02:07:32 -0400

    adding ability to add concerts

--- app/addcon/[addconId]/conAdd.ts
+++ app/addcon/[addconId]/conAdd.ts
@@ -20,22 +20,11 @@ export async function conAdd(previousState: string, formData: FormData){
 
     const supabase = await createClient();
 
-    if(showdate == ""){
-        const { error } = await supabase
-            .from('concert_concerts')
-            .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue})
-        if (error)
-            return "Error: " + error.code + " : " + error.message;
-        else
-            return "Successfully created!";
-    }
-    else {
-        const { error } = await supabase
-            .from('concert_concerts')
-            .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue, date_of: showdate})
-        if (error)
-            return "Error: " + error.code + " : " + error.message;
-        else
-            return "Successfully created!";
-    }
+    const { error } = await supabase
+        .from('concert_concerts')
+        .insert({ user_id: user, headliner: head, other_artists: other, tour_name: tour, venue: venue})
+    if (error)
+        return "Error: " + error.code + " : " + error.message;
+    else
+        return "Successfully created!";
 }
