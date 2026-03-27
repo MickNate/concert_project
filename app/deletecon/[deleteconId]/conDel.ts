89fa41c17dba9b8d3e96e948833b629677ce3179
MickNate <michaelnathanmitchell@gmail.com>
2026-03-27 00:16:33 -0400

    adding ability to delete concerts

--- app/deletecon/[deleteconId]/conDel.ts
+++ app/deletecon/[deleteconId]/conDel.ts
@@ -6,9 +6,10 @@ export async function conDel(previousState: string, formData: FormData){
     await new Promise((resolve) => setTimeout(resolve, 2000));
     const delInput = formData.get("userinput") as string;
     const conId = formData.get("conparam") as string;
+    const deleteMessage = "DELETE";
 
-    if(delInput != "DELETE")
-        return "Error: Input does not match DELETE"
+    if(delInput != deleteMessage)
+        return "Error:" + delInput + "does not match DELETE"
 
     const supabase = await createClient();
     const { data, error } = await supabase
