83fdee4201849c6eb3f14a4fa5aff70a3620d4e3
MickNate <michaelnathanmitchell@gmail.com>
2026-04-20 01:09:08 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -8,48 +8,28 @@ export async function searchRes(previousState: string, formData: FormData){
 
     if(artist == null || artist == "")
         return "Nothing searched";
-    else{
-        const input =  "'" + artist.replace(" ", "' & '") + "'";
+    else {
+        const input = "'" + artist.replace(" ", "' & '") + "'";
 
         const supabase = await createClient();
-        const { data: concert, error} = await supabase
+        const {data: concert, error} = await supabase
             .from("concert_concerts")
             .select('user_id')
-            .textSearch('headliner', input, { type: 'phrase', config: 'english'})
+            .textSearch('headliner', input, {type: 'phrase', config: 'english'})
             .select('user_id')
-            .textSearch('other_artists', input, { type: 'phrase', config: 'english'})
+            .textSearch('other_artists', input, {type: 'phrase', config: 'english'})
 
-        if(error){
+        if (error) {
             return "Error for concert table: " + error.code + " : " + error.message;
         }
-
-        console.log("Made it pass the concert table.");
-
-        if(concert == null || concert.toString() == " "){
-            return "No users found that have attended this artist.";
+        if(concert == null){
+            return "No user appears to have attended this concert.";
         }
+
         else{
-            const supabase = await createClient();
-
-            console.log("Starting the user table.");
-
-            const found = [];
-
-            for (let i = 0; i < concert.length; i++) {
-                const {data: user, error} = await supabase
-                    .from("concert_users")
-                    .select('username')
-                    .eq('user_id',concert[i].user_id)
-                if(user != null)
-                    found.push(user)
-                if(error){
-                    return "Error for user table: " + error.code + " : " + error.message;
-                }
-            }
-            console.log("Made it pass the user table.");
-
-            return "The output is " + concert[0].user_id;
+            return "We found " + concert[0].user_id + " attended.";
         }
 
     }
+
 }
