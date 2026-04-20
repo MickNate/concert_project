8ca59176a10bec0f2bc123f4e403a1751a883e22
MickNate <michaelnathanmitchell@gmail.com>
2026-04-20 00:26:12 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -48,7 +48,7 @@ export async function searchRes(previousState: string, formData: FormData){
             }
             console.log("Made it pass the user table.");
 
-            return found;
+            return found[0];
         }
 
     }
