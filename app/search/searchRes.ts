3ae36b2bd4fca62245563ab68ac6278955a2c9b2
MickNate <michaelnathanmitchell@gmail.com>
2026-04-20 01:22:28 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -27,7 +27,7 @@ export async function searchRes(previousState: string, formData: FormData){
         }
 
         else{
-            return "We found " + concert[0].user_id + " attended.";
+            return "We found " + {concert} + " attended.";
         }
 
     }
