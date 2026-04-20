53edc309acc9337579d5686b1b9aa9f2ff7d7885
MickNate <michaelnathanmitchell@gmail.com>
2026-04-19 23:46:40 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -21,19 +21,23 @@ export async function searchRes(previousState: string, formData: FormData){
             return "Error for concert table: " + error.code + " : " + error.message;
         }
 
+        console.log("Made it pass the concert table.");
+
         if(concert == null){
             return "No users found that have attended this artist.";
         }
-        if(concert[0].user_id == null){
-            return "User Id not accessible";
-        }
         else{
             const supabase = await createClient();
+
+            console.log("Starting the user table.");
 
             const {data: user, error} = await supabase
                 .from("concert_users")
                 .select('username')
                 .eq('user_id',concert[0].user_id)
+
+            console.log("Made it pass the user table.");
+
             if(error){
                 return "Error for user table: " + error.code + " : " + error.message;
             }
