057933e10aa5ffca11973872e2ea2e13e9081763
MickNate <michaelnathanmitchell@gmail.com>
2026-04-19 23:34:47 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -5,15 +5,17 @@ import {createClient} from "@/utils/supabase/server";
 export async function searchRes(previousState: string, formData: FormData){
     await new Promise((resolve) => setTimeout(resolve, 2000))
     const artist = formData.get("value") as string;
+
     if(artist == null || artist == "")
         return "Nothing searched";
-
     else{
+        const input =  "'" + artist.replace(" ", " & ") + "'";
+
         const supabase = await createClient();
         const { data: concert, error} = await supabase
             .from("concert_concerts")
             .select('user_id')
-            .textSearch('headliner', artist, { type: 'phrase', config: 'english'})
+            .textSearch('headliner', input, { type: 'phrase', config: 'english'})
 
         if(error){
             return "Error for concert table: " + error.code + " : " + error.message;
