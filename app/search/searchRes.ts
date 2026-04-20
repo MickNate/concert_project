441d74173d644a90b9ffe8edbe19ba6f1271ae54
MickNate <michaelnathanmitchell@gmail.com>
2026-04-20 02:53:58 -0400

    adding search function

--- app/search/searchRes.ts
+++ app/search/searchRes.ts
@@ -8,44 +8,44 @@ export async function searchRes(previousState: string, formData: FormData){
 
     if(artist == null || artist == "")
         return "Nothing searched";
-    else {
-        const input = "'" + artist.replace(" ", "' & '") + "'";
 
-        const supabase = await createClient();
-        const {data: concert, error} = await supabase
-            .from("concert_concerts")
-            .select('user_id')
-            .textSearch('headliner', input, {type: 'phrase', config: 'english'})
+    const input = "'" + artist.replace(" ", "' & '") + "'";
 
-        if (error) {
-            return "Error for concert table: " + error.code + " : " + error.message;
+    const supabase = await createClient();
+    const {data: concert, error} = await supabase
+        .from("concert_concerts")
+        .select('user_id')
+        .textSearch('headliner', input, {type: 'phrase', config: 'english'})
+
+    if (error)
+        return "Error for concert table: " + error.code + " : " + error.message;
+
+    const {data: otherart} = await supabase
+        .from("concert_concerts")
+        .select('user_id')
+        .textSearch('other_artists', input, {type: 'phrase', config: 'english'})
+
+    if (error)
+        return "Error for concert table.";
+
+    const results = [];
+    if(concert != null){
+        for(let i = 0; i < concert.length; i++){
+            results.push(concert[i].user_id);
         }
-
-        const {data: otherart} = await supabase
-            .from("concert_concerts")
-            .select('user_id')
-            .textSearch('other_artists', input, {type: 'phrase', config: 'english'})
-
-        if (error) {
-            return "Error for concert table.";
-        }
-
-        else{
-            const results = [];
-            if(concert != null){
-                for(let i = 0; i < concert.length; i++){
-                    results.push(concert[i].user_id);
-                }
-            }
-            if(otherart != null){
-                for(let j = 0; j < otherart.length; j++){
-                    if(results.includes(otherart[j].user_id))
-                        results.push(concert[j].user_id);
-                }
-            }
-            return "Results are " + results;
-        }
-
     }
+    if(otherart != null){
+        for(let j = 0; j < otherart.length; j++){
+            if(results.includes(otherart[j].user_id))
+                results.push(concert[j].user_id);
+        }
+    }
+
+    if(results.length > 0)
+        return "Nobody has attended a concert of this artist."
+
+
+
+    return "Results are " + results;
 
 }
